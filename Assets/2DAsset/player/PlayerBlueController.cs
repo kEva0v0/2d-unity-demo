8c626a7e1d116e33e8554589f64123b78ea85d73
kuakuakua <745462171@qq.com>
2023-03-29 19:31:50 +0800

    feature:test blue player

---
+++ Assets/2DAsset/player/PlayerBlueController.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBlueController : MonoBehaviour
+{
+    private Rigidbody2D rb2D;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
