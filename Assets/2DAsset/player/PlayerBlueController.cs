68e09dbd0365cf59b586813d294386edb5687f06
kuakuakua <745462171@qq.com>
2023-03-30 00:34:24 +0800

    test

--- Assets/2DAsset/player/PlayerBlueController.cs
+++ Assets/2DAsset/player/PlayerBlueController.cs
@@ -10,7 +10,7 @@ public class PlayerBlueController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb2D = gameObject.GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
