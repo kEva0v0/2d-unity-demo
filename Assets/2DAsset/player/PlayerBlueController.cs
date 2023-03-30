763a7a7a690927f7870c3b2e882b172fd350fbc2
kuakuakua <745462171@qq.com>
2023-03-30 21:43:08 +0800

    feature:增加跳跃&Trail Renderer

--- Assets/2DAsset/player/PlayerBlueController.cs
+++ Assets/2DAsset/player/PlayerBlueController.cs
@@ -2,39 +2,52 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerBlueController : MonoBehaviour
-{
-    private Rigidbody2D rb2D;
+public class PlayerBlueController : MonoBehaviour {
+	private Rigidbody2D blueRb2D;
+	private Collider2D blueCo2D;
+	public LayerMask groundLayer;
 
-    private float moveSpeed;
-    private float jumpForce;
-    private bool isJumping;
+	private float moveSpeed;
+	private float jumpForce = 5f;
+	private float checkDistance = 0.1f;
+	private bool isJumping;
 
-    private float moveHorizontal;
-    private float moveVertical;
+	private float moveHorizontal;
+	private bool inputJump;
 
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        rb2D = gameObject.GetComponent<Rigidbody2D>();
-        moveSpeed = 1f;
-        isJumping = false;
-        jumpForce = 60f;
-    }
+	// Start is called before the first frame update
+	void Start ()
+	{
+		blueRb2D = gameObject.GetComponent<Rigidbody2D> ();
+		blueCo2D = gameObject.GetComponent<Collider2D> ();
+		moveSpeed = 1f;
+	}
 
-    // Update is called once per frame
-    void Update()
-    {
-        moveHorizontal = Input.GetAxisRaw("Horizontal");
-        moveVertical = Input.GetAxisRaw("Vertical");
-    }
+	// Update is called once per frame
+	void Update ()
+	{
+		moveHorizontal = Input.GetAxisRaw ("Horizontal");
+		inputJump = Input.GetButtonDown ("Jump");
+		// jump
+		if (inputJump && IsGrounded ()) {
+			blueRb2D.AddForce (Vector2.up * jumpForce, ForceMode2D.Impulse);
+		}
+	}
 
-    private void FixedUpdate()
-    {
-        if (moveHorizontal < -0.1f || moveHorizontal > 0.1f)
-        {
-            rb2D.AddForce(new Vector2(moveHorizontal * moveSpeed, 0f), ForceMode2D.Impulse);
-        }
-    }
+	private bool IsGrounded ()
+	{
+		Vector2 boxCastSize = new Vector2 (blueCo2D.bounds.size.x, checkDistance);
+		RaycastHit2D hit = Physics2D.BoxCast (blueCo2D.bounds.center, boxCastSize, 0, Vector2.down, blueCo2D.bounds.extents.y + checkDistance, groundLayer);
+		return hit.collider != null;
+	}
+
+	private void FixedUpdate ()
+	{
+		// move horizontal
+		if (moveHorizontal < -0.1f || moveHorizontal > 0.1f) {
+			blueRb2D.AddForce (Vector2.right * moveHorizontal * moveSpeed, ForceMode2D.Impulse);
+		}
+
+	}
 }
---
+++ Assets/2DAsset/shader/NewShader.shader
@@ -0,0 +1,48 @@
+﻿Shader "NewShader" 
+{
+    Properties
+    {
+        _MainTex ("Base (RGB)", 2D) = "white" {}
+        _Color ("Tint", Color) = (1, 1, 1, 1)
+        _AfterimageDuration ("Afterimage Duration", Range(0, 1)) = 0.5
+        _Speed ("Speed", Range(0, 10)) = 2
+    }
+	SubShader
+    {
+        Tags {"Queue"="Transparent" "RenderType"="Transparent"}
+        LOD 100
+
+        CGPROGRAM
+        #pragma surface surf Lambert
+        #pragma target 4.5
+
+        sampler2D _MainTex;
+        float4 _Color;
+        float _AfterimageDuration;
+        float _Speed;
+
+        struct Input
+        {
+            float2 uv_MainTex;
+        };
+
+        void surf (Input IN, inout SurfaceOutput o)
+        {
+            float4 c = tex2D(_MainTex, IN.uv_MainTex) * _Color;
+            o.Albedo = c.rgb;
+
+            // Create afterimage effect
+            float ghostFade = 0;
+            for (int i = 1; i <= 10; i++)
+            {
+                float2 ghostUV = IN.uv_MainTex + float2(0, _Speed * _AfterimageDuration * i * 0.01);
+                float4 ghostColor = tex2D(_MainTex, ghostUV) * _Color;
+                ghostFade += (_AfterimageDuration * ghostColor.a) / (i * 1.0);
+            }
+            o.Alpha = c.a + ghostFade;
+        }
+        ENDCG
+    }
+    FallBack "UniversalRP/2D/Sprite-Lit-Default"
+}
+
